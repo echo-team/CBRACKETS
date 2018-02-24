7bab950ea81fad6a86218716552cff169b693d67
Илья Колдунов <gigafiga21@ECHO.HP620>
2018-02-24 15:20:08 +0300

    Added icon module (issue #1).

---
+++ js/icon.js
@@ -0,0 +1,5 @@
+define(
+    function(require, exports, module)
+    {
+        let icon = $("<a class='CBRACKETS__ICON' href='#'></a>").appendTo("#main-toolbar .buttons");
+    });
--- main.js
+++ main.js
@@ -10,12 +10,16 @@ define(
                 utils:
                 {
                     appInit: brackets.getModule("utils/AppInit")
+                },
+                modules:
+                {
+                    icon: null
                 }
             };
 
         environment.utils.appInit.appReady(
             function()
             {
-
+                environment.modules.icon = require("JS/icon");
             });
     });
