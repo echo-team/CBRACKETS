6179429b0cabc60b1de9b76788979ef4aa12c5b1
Илья Колдунов <gigafiga21@ECHO.HP620>
2018-02-24 16:16:20 +0300

    Added CSS class to the panel module (issue#1).

--- js/panel.js
+++ js/panel.js
@@ -3,9 +3,11 @@ define(
     {
         let id = "CBRACKETS.panel",
             panel = null,
-            WorkspaceManager = brackets.getModule("view/WorkspaceManager");
+            WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
+            ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
 
+        ExtensionUtils.loadStyleSheet(module, "../css/panel.css");
         panel = WorkspaceManager.createBottomPanel(id, $(require("text!../html/panel.html")), 200);
 
         return panel;
