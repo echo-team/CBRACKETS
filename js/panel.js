5df306113b8090856fbf88afba56da84a4c2e8ac
Илья Колдунов <gigafiga21@ECHO.HP620>
2018-02-25 14:52:55 +0300

    Added buttons (issue #1).

--- js/panel.js
+++ js/panel.js
@@ -2,13 +2,17 @@ define(
     function(require, exports, module)
     {
         let id = "CBRACKETS.panel",
-            panel = null,
-            WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
+            panel = null;
+
+        let WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
             ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
 
         ExtensionUtils.loadStyleSheet(module, "../css/panel.css");
         panel = WorkspaceManager.createBottomPanel(id, $(require("text!../html/panel.html")), 200);
+
+        $('<img src="' + require.toUrl("../icons/play.svg") + '">').appendTo($('[class="btn CBRACKETS__BUTTON"]')[0]);
+        $('<img src="' + require.toUrl("../icons/stop.svg") + '">').appendTo($('[class="btn CBRACKETS__BUTTON"]')[1]);
 
         return panel;
     });
